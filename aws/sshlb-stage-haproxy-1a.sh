946ef7044972483cf5f5b1b7301d9f5be7081710
SouthernAirTemple <herp@contrastring.org>
2015-08-24 13:47:14 -0500

    Added cloud-init info for hostname change

--- aws/sshlb-stage-haproxy-1a.sh
+++ aws/sshlb-stage-haproxy-1a.sh
@@ -3,3 +3,8 @@ sh -c "echo '# /etc/cloud/cloud.cfg.d/defaults.cfg
 # hostname reverts if not added 
 hostname: sshlb-stage-haproxy-1a   
 fqdn: sshlb-stage-haproxy-1a.example.com' > /etc/cloud/cloud.cfg.d/defaults.cfg"
+
+# remove initial config and rerun with new defaults
+rm -fr /var/lib/cloud/instances
+
+cloud-init init  
