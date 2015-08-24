ed82d3c3da7de1ceb5e4cf9df1fc6a93545e3639
SouthernAirTemple <herp@contrastring.org>
2015-08-23 19:26:36 -0500

    Added sshlb-stage-haproxy-1a script and more tutorial info

---
+++ aws/sshlb-stage-haproxy-1a.sh
@@ -0,0 +1,5 @@
+sudo sh -c "echo '# /etc/cloud/cloud.cfg.d/defaults.cfg
+# cloud-config
+# hostname reverts if not added 
+hostname: sshlb-stage-haproxy-1a   
+fqdn: sshlb-stage-haproxy-1a.example.com' > /etc/cloud/cloud.cfg.d/defaults.cfg"
