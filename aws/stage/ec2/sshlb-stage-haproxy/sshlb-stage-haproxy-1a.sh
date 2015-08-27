adcd5056123f97b69b24839bc4663d36ca4d706e
SouthernAirTemple <herp@contrastring.org>
2015-08-27 15:06:29 -0500

    Modified sshd config

--- aws/stage/ec2/sshlb-stage-haproxy/sshlb-stage-haproxy-1a.sh
+++ aws/stage/ec2/sshlb-stage-haproxy/sshlb-stage-haproxy-1a.sh
@@ -12,10 +12,15 @@ cloud-init init
 # turn off selinux as is active with ami
 sed 's/SELINUX=enabled/SELINUX=disabled/g' -i /etc/selinux/config
 
+# ensure SSH is listening on port 2222 rather than 22 for forwarding
+sed 's/#Port 22/Port 2222/g' /etc/ssh/sshd_config
+
+systemctl restart sshd 
+
 setenforce 0
 
 yum install haproxy-1.5.4-4.el7_1 -y
 
 yum install vim -y
 
-
+systemctl start haproxy
