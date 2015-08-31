b4be36fa18a6168d5e06d87d93d7a4b653ef1648
SouthernAirTemple <herp@contrastring.org>
2015-08-31 11:06:07 -0500

    Added puppet stage 1a script info

--- aws/stage/ec2/config-stage-puppet/config-stage-puppet-1a.sh
+++ aws/stage/ec2/config-stage-puppet/config-stage-puppet-1a.sh
@@ -8,3 +8,20 @@ fqdn: config-stage-puppet-1a.internal.example.com' > /etc/cloud/cloud.cfg.d/defa
 rm -fr /var/lib/cloud/instances
 
 cloud-init init  
+
+yum install ntp-4.2.6p5-19.el7_1.1
+
+yum install vim -y
+
+yum install screen -y
+
+sh -c "echo '#!/bin/sh
+/usr/sbin/ntpdate pool.ntp.org' > /etc/cron.daily/ntpdate"
+
+rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
+
+yum clean all
+
+yum install puppet-server 3.8.2-1.el7 -y
+
+screen -d -m puppet master --verbose --no-daemonize
