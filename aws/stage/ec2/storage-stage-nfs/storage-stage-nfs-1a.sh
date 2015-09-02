119ea923382f039109275b5d2a134fd5c51af169
SouthernAirTemple <herp@contrastring.org>
2015-09-02 17:01:20 -0500

    Added nfs server defaults info

--- aws/stage/ec2/storage-stage-nfs/storage-stage-nfs-1a.sh
+++ aws/stage/ec2/storage-stage-nfs/storage-stage-nfs-1a.sh
@@ -1,3 +1,4 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sh -c "echo '# /etc/cloud/cloud.cfg.d/defaults.cfg
 # cloud-config
 # hostname reverts if not added 
@@ -8,3 +9,4 @@ fqdn: storage-stage-nas-1a.internal.example.com' > /etc/cloud/cloud.cfg.d/defaul
 rm -fr /var/lib/cloud/instances
 
 cloud-init init  
+bash "$DIR"/../defaults.sh
