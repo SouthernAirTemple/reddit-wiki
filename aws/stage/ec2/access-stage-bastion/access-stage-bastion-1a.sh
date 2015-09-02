59d14ae8f96965a5c4b2c95344c8c28aa8a462c1
SouthernAirTemple <herp@contrastring.org>
2015-09-02 14:29:34 -0500

    Fixed cloud-init by adding init

--- aws/stage/ec2/access-stage-bastion/access-stage-bastion-1a.sh
+++ aws/stage/ec2/access-stage-bastion/access-stage-bastion-1a.sh
@@ -8,6 +8,6 @@ fqdn: access-stage-bastion-1a.internal.example.com' > /etc/cloud/cloud.cfg.d/def
 # remove initial config and rerun with new defaults
 rm -fr /var/lib/cloud/instances
 
-cloud-init
+cloud-init init
 
 bash "$DIR"/../defaults.sh
