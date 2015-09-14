e8659d030b816242f376da971310b8e0b714e85b
SouthernAirTemple <herp@contrastring.org>
2015-09-14 10:26:44 -0500

    Changed nas to nfs for hostname

--- aws/stage/ec2/storage-stage-nfs/storage-stage-nfs-1a.sh
+++ aws/stage/ec2/storage-stage-nfs/storage-stage-nfs-1a.sh
@@ -2,8 +2,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sh -c "echo '# /etc/cloud/cloud.cfg.d/defaults.cfg
 # cloud-config
 # hostname reverts if not added 
-hostname: storage-stage-nas-1a   
-fqdn: storage-stage-nas-1a.internal.example.com' > /etc/cloud/cloud.cfg.d/defaults.cfg"
+hostname: storage-stage-nfs-1a   
+fqdn: storage-stage-nfs-1a.internal.example.com' > /etc/cloud/cloud.cfg.d/defaults.cfg"
 
 # remove initial config and rerun with new defaults
 rm -fr /var/lib/cloud/instances
